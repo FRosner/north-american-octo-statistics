1055977b31e5ba612ed72729cf2f3e64d08f66dd
Frank Rosner <frank@fam-rosner.de>
2014-12-14 00:49:32 +0100

    first version

---
+++ src/de/famrosner/githubstatistics/Launch.scala
@@ -0,0 +1,59 @@
+package de.famrosner.githubstatistics
+
+import com.google.gson.reflect.TypeToken
+import com.google.gson.{JsonObject, JsonArray, Gson}
+
+import scalaj.http.Http
+import scalaj.http.HttpResponse
+
+import java.lang.reflect.Type
+
+import scalaz._
+import Scalaz._
+
+import scala.collection.JavaConverters._
+import scala.collection.immutable.HashMap
+
+import scala.util.parsing.json._
+
+
+object Launch {
+
+  case class Repository(val id: Long, val full_name: String)
+
+  class LanguageTypeToken extends TypeToken[java.util.Map[String, Double]]
+
+  val gson = new Gson()
+  val languageMapType: Type = new LanguageTypeToken().getType();
+
+  val repositoriesUrl = "https://api.github.com/repositories"
+
+  /**
+    *  %s = full_name of repository
+   *  Example: https://api.github.com/repos/mojombo/grit/languages
+   */
+  val languageUrl = "https://api.github.com/repos/%s/languages"
+
+  def main(args: Array[String]): Unit = {
+    def repo2language(response: HttpResponse[String]): java.util.Map[String, Double] = {
+      gson.fromJson(response.body, languageMapType)
+    }
+    def mergeMap[A, B](maps: Iterable[Map[A, B]])(f: (B, B) => B): Map[A, B] =
+      (Map[A, B]() /: (for (map <- maps; kv <- map) yield kv)) { (a, kv) =>
+        a + (if (a.contains(kv._1)) kv._1 -> f(a(kv._1), kv._2) else kv)
+      }
+
+    val repositoriesResponse = Http(repositoriesUrl).asString
+    val repositories: Array[Repository] = gson.fromJson(repositoriesResponse.body, classOf[Array[Repository]]).take(5)
+    val languagesResponse = repositories.map(repository => Http(String.format(languageUrl, repository.full_name)).asString)
+    val languagesJava: Iterable[java.util.Map[String, Double]] = languagesResponse.map(repo2language)
+    val languagesScala = languagesJava.map(HashMap() ++ _.asScala)
+    val languageSummaryBytes = mergeMap(languagesScala)(_ + _)
+    val byteSum: Double = languageSummaryBytes.values.sum
+    val languageSummaryPercentage = languageSummaryBytes.map{
+      case (language, bytes) => (language, bytes / byteSum)
+    }
+    languageSummaryPercentage foreach println
+  }
+
+}
